261e87713c6c816a09b4623a319a6a8381e4c318
janseweryn <janseweryn15@gmail.com>
2026-08-10 23:46:25 +0200

    daily

--- src/App.jsx
+++ src/App.jsx
@@ -4269,6 +4269,32 @@ const manualDaily = {
       dailyCategory: "Rap",
     },
   ],
+    "2026-08-11": [
+    {
+      title: "Rather Be (feat. Jess Glynne)",
+      artist: "Clean Bandit",
+      cover: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/19/e0/1b/19e01b5f-0fd5-85f1-f347-461d4130309a/825646029594.jpg/600x600bb.jpg",
+      snippet: "/songs/pop/clean_bandit_rather_be_feat_jess_glynne.mp3",
+      categories: ["pop"],
+      dailyCategory: "Pop",
+    },
+    {
+      title: "People Are Strange",
+      artist: "The Doors",
+      cover: "/songs/covers/strange.jpg",
+      snippet: "/songs/rock/strange_people.mp3",
+      categories: ["rock"],
+      dailyCategory: "Rock",
+    },
+{
+      title: "Lollipop (feat. Static Major)",
+      artist: "Lil Wayne",
+      cover: "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/ad/59/2b/ad592bdf-0fd7-857f-3ca5-12f1d4861e3e/08UMGIM15512.rgb.jpg/600x600bb.jpg",
+      snippet: "/songs/rap/lil_wayne_lollipop_feat_static_major.mp3",
+      categories: ["rap"],
+      dailyCategory: "Rap",
+    },
+  ],
 };
 
 const LEVELS = [
